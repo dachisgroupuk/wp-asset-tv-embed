6eb0ff96e099c7be666b459534140d561d61cff6
Digitales <ross.tweedie@dachisgroup.com>
2013-08-18 17:25:24 +0100

    Ensure the correct methods are declared as static.

--- classes/wpate-admin.php
+++ classes/wpate-admin.php
@@ -24,7 +24,7 @@ class WpAssetTvEmbedAdmin extends WpAssetTvEmbedCore
      * @param void
      * @return void
      */
-    function add_admin_pages()
+    static function add_admin_pages()
     {
         add_media_page( __( 'Asset TV Embed' , 'imv'), __( 'Asset TV Embed' , 'imv'), 'create_users', 'asset-tv-embed', array( __CLASS__, 'index_page' ) );
     }
